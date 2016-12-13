128693f439d9f8f2fad91985fbf6d50fbf48a15b
August Alfredsson <august.alfredsson@gmail.com>
2016-12-13 14:04:05 +0100

    done

--- src/main/GradeHack.java
+++ src/main/GradeHack.java
@@ -19,58 +19,85 @@ import javax.crypto.spec.SecretKeySpec;
 public class GradeHack {
 
 	public GradeHack(String name, String grade) {
-		byte[] signature = new byte[10];
-		
-		
-		
+
+		String[] alphanumeric = { "0", "1", "2", "3", "4", "5", "6", "7", "8",
+				"9", "a", "b", "c", "d", "e", "f" };
+		String signature = "";
+		long threshold = 19000000;
+
+		long timetime = System.currentTimeMillis();
 		try {
-			
-			
-			
-			for (int j = 0; j < 10; j++) {
-				byte realValue = (byte) 0;
-				long longestResponse = 0;
-				//Make first connection so all after this one are "equal"
+
+			for (int j = 0; j < 20; j++) {
+
+				// Make first connection so all after this one are "equal".
+				// Maybe not needed
 				String url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?";
-				url += "name=" + name + "&grade=" + grade + "&signature=00000000000000000000";
+				url += "name=" + name + "&grade=" + grade;
 				URL u2 = new URL(url);
-				System.out.println("URL: " + url);
 
 				URLConnection uc2 = u2.openConnection();
-				for (int i = 0; i < 256; i++) {
-					
-					byte testValue = (byte) i;
-					byte[] tempsig = signature;
-					tempsig[j] = testValue;
-					
-					
-					String sig = printByteArray(tempsig);
-					
-					String urlTest = url + "&signature=" + sig;
-					URL u = new URL(urlTest);
-					System.out.println("URL: " + urlTest);
-					long startTime = System.nanoTime();
-					URLConnection uc = u.openConnection();
-					BufferedReader br = new BufferedReader(
-							new InputStreamReader(uc.getInputStream(), "UTF-8"));
-					String line;
-					while ((line = br.readLine()) != null) {
-						System.out.println("Read: " + line);
-						if(line.equals("1")) {
-							System.out.println();
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(
+						uc2.getInputStream(), "UTF-8"));
+
+				long longestResponse = 60000000;
+
+				String pChar = ""; // This variable will hold a value that could
+									// possibly be the correct one
+				for (int i = 0; i < 16; i++) {
+					int it = 0;
+					long response = Long.MAX_VALUE;
+					String tempsig = signature;
+					tempsig += alphanumeric[i];
+					while (it < 2) {
+						String urlTest = url + "&signature=" + tempsig;
+						URL u = new URL(urlTest);
+						// System.out.println("URL: " + urlTest);
+						long startTime = System.nanoTime();
+						URLConnection uc = u.openConnection();
+						BufferedReader br = new BufferedReader(
+								new InputStreamReader(uc.getInputStream(),
+										"UTF-8"));
+						String line;
+						while ((line = br.readLine()) != null) {
+							// System.out.println("Read: " + line);
+							if (line.equals("1")) {
+								System.out.println("CRACKED");
+								
+							}
 						}
+						long responseTime = System.nanoTime() - startTime;
+						if (responseTime < response) {
+							response = responseTime;
+							System.out.println("Response: " + response);
+							// System.out.println("Shortest response: " +
+							// response + " and longest: " + longestResponse);
+						}
+						it++;
 					}
-					long responseTime = System.nanoTime() - startTime;
-					if(responseTime > longestResponse && i != 0) {
-						longestResponse = responseTime;
-						realValue = testValue;
-						//signature = tempsig;
+
+					System.out.println("Responstid på " + tempsig + " var "
+							+ response);
+					// Kollar om responstiden på det här talet var längre än
+					// tidigare tal
+					if (response > longestResponse) {
+						long diff = response - longestResponse;
+						System.out.println("Diff: " + diff
+								+ " när tempsig var: " + tempsig + " och i = "
+								+ i);
+
+						System.out.println("2.");
+						pChar = alphanumeric[i];
+						longestResponse = response;
+
 					}
+
 				}
-				signature[j] = realValue;
+				signature += pChar;
+
 			}
-			System.out.println("Signaturen: ");
-			printByteArray(signature);
+			System.out.println("Signaturen: " + signature);
+
 			// StringSelection urlToClipboard = new StringSelection(url);
 			// Clipboard clipboard = Toolkit.getDefaultToolkit()
 			// .getSystemClipboard();
@@ -80,6 +107,9 @@ public class GradeHack {
 			e.printStackTrace();
 		}
 
+		long time3 = System.currentTimeMillis() - timetime;
+
+		System.out.println("Det tog " + time3 / 60000 + " minuter! (" + time3 + " ms)");
 	}
 
 	private String printByteArray(byte[] array) {
